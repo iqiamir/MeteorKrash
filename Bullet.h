a656aebe07640c8643fee21f415e38c54bfd4ae0
Rifqi Amir <75501059+iqiamir@users.noreply.github.com>
2022-07-27 16:02:17 +0800

    Create Bullet.h

---
+++ Bullet.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Bullet
+{
+private:
+
+	sf::Sprite shape;
+	
+
+	sf::Vector2f direction;
+	float movementSpeed;
+
+public:
+	Bullet();
+	Bullet(sf::Texture* texture,float pos_x, float pos_y, float dir_x, float dir_y, float movement_speed);
+	virtual ~Bullet();
+
+	//Acces to the game position /Accesor
+	const sf::FloatRect getBounds() const;
+
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
+#endif // !BULLET_H
